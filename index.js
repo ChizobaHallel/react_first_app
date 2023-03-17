00b985f6550302fb17c9e1c8bb1019ce91890d9b
Hallel-Praise Chizoba <cp3badmus@gmail.com>
2023-03-17 07:35:11 +0100

    i made changes to my index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// const React = require("react");
+// const ReactDOM = require("react-dom");
+
+
+// const element = React.createElement("h1", null, "Sup peoples !!!!!");
+// ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(<ul>
+    <li>Hi my name is Hallel</li> 
+    <li>who are you</li>
+    <li>why are you alive</li>
+    </ul>, document.getElementById("root"))
