33a140ecb65e3d136c05fb3c9873a608b91804e4
Hallel-Praise Chizoba <cp3badmus@gmail.com>
2023-03-17 22:46:36 +0100

    i made changes to my index.js by creating a navbar

--- index.js
+++ index.js
@@ -4,8 +4,14 @@
 
 // const element = React.createElement("h1", null, "Sup peoples !!!!!");
 // ReactDOM.render(element, document.getElementById("root"));
-ReactDOM.render(<ul>
-    <li>Hi my name is Hallel</li> 
-    <li>who are you</li>
-    <li>why are you alive</li>
-    </ul>, document.getElementById("root"))
+// ReactDOM.render(<ul>
+//     <li>Hi my name is Hallel</li> 
+//     <li>who are you</li>
+//     <li>why are you alive</li>
+//     </ul>, document.getElementById("root"))
+
+// const h1 = document.createElement("h1")
+// h1.textContent = "This is an imperative way to program"
+// h1.className = "header"
+// document.getElementById("root").append(h1)
+
