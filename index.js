2677f25f12b022ff35e713f7518b038bd721ed0e
Hallel-Praise Chizoba <cp3badmus@gmail.com>
2023-03-26 01:12:11 +0100

    i created a navbar for small screens

--- index.js
+++ index.js
@@ -10,8 +10,8 @@
 //     <li>why are you alive</li>
 //     </ul>, document.getElementById("root"))
 
-// const h1 = document.createElement("h1")
-// h1.textContent = "This is an imperative way to program"
-// h1.className = "header"
-// document.getElementById("root").append(h1)
+const h1 = document.createElement("h1")
+h1.textContent = "This is an imperative way to program"
+h1.className = "header"
+document.getElementById("root").append(h1)
 
